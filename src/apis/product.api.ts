393127c890422ef8d7f42efdb454a54610febf67
minhtan0804 <minhtan290902@gmail.com>
2023-02-03 09:50:20 +0700

    code ui product detail page

---
+++ src/apis/product.api.ts
@@ -0,0 +1,17 @@
+import { URL_GET_PRODUCTS_DETAIL, URL_GET_PRODUCTS } from '../constants/url'
+import apiService from '../services'
+
+export const productService = {
+  /* Calling the apiService.get method and passing in the URL_GETPRODUCTS constant. */
+  getProducts() {
+    return apiService.get(URL_GET_PRODUCTS)
+  },
+  /**
+   * It returns the product details of the product with the given id
+   * @param {string} id - The id of the product you want to get details for.
+   * @returns The product details
+   */
+  getProductsDetails(id: string) {
+    return apiService.get(`${URL_GET_PRODUCTS_DETAIL}/${id}`)
+  }
+}
--- src/pages/ProductDetail/ProductDetails.tsx
+++ src/pages/ProductDetail/ProductDetails.tsx
@@ -1,7 +1,190 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom'
+import { productService } from '../../apis/product.api'
+import { Button, Divider, Image, Rate } from 'antd'
+import {
+  AddOrBuy,
+  ButtonCustom,
+  ContentWrapper,
+  ProductImage,
+  ProductImageDiff,
+  ProductImageShow,
+  ProductInfo,
+  ProductWrapper,
+  Quantity,
+  QuantityWrapper,
+  StyledButton,
+  StyledCategory,
+  StyledH2,
+  StyledH3,
+  StyledRating,
+  StyledShare,
+  StyledSpan,
+  SubImage,
+  TabsCustom
+} from './productDetailStyle'
+import {
+  FacebookOutlined,
+  GoogleOutlined,
+  HeartOutlined,
+  MinusOutlined,
+  PlusOutlined,
+  WhatsAppOutlined
+} from '@ant-design/icons'
+import Description from './components/Description'
+import Reviews from './components/Reviews'
+import RelatedProduct from './components/RelatedProduct'
+import ThumbsImage from './components/ThumbsImage'
 
 const ProductDetails = () => {
-  return <div>ProductDetails</div>
+  const rel = []
+  const { id } = useParams()
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['productDetail', id],
+    queryFn: () => productService.getProductsDetails(id as string)
+  })
+
+  console.log(data, isLoading, isError)
+
+  const [quantity, setQuantity] = useState(1)
+  const [like, setLike] = useState(false)
+
+  const handleDecrease = () => {
+    setQuantity((prev) => prev - 1)
+  }
+
+  const handleIncrease = () => {
+    setQuantity((prev) => prev + 1)
+  }
+
+  const handleLike = () => {
+    setLike((prev) => !prev)
+  }
+
+  return (
+    <ContentWrapper>
+      <ProductWrapper>
+        <>
+          <ProductImage
+            style={{
+              width: '50%'
+            }}
+          >
+            <ProductImageShow>
+              <Image width={'100%'} height={'100%'} style={{ objectFit: 'cover' }} src={data?.data.imgUrl[0].url} />
+            </ProductImageShow>
+            <ProductImageDiff>
+              {data?.data.imgUrl.slice(1, 5).map((itemImage: any) => (
+                <SubImage key={itemImage.id}>
+                  <Image
+                    width={'100%'}
+                    height={'100%'}
+                    src={itemImage.url}
+                    style={{
+                      borderRadius: '16px',
+                      overflow: 'hidden',
+                      objectFit: 'cover'
+                    }}
+                  />
+                </SubImage>
+              ))}
+            </ProductImageDiff>
+
+            {/* <ThumbsImage dataImage = {data?.data.imgUrl} /> */}
+          </ProductImage>
+        </>
+
+        <ProductInfo>
+          <>
+            <StyledH2>{data?.data.productName}</StyledH2>
+            <StyledH3>${data?.data.price}</StyledH3>
+            <StyledRating>
+              <Rate allowHalf disabled value={data?.data.avgRating} />
+              <StyledSpan style={{ fontWeight: '0.8rem' }}>
+                {data?.data.reviews.length === 0 ? 'No reviews' : `${data?.data.reviews.length} reviews`}
+              </StyledSpan>
+              <StyledSpan style={{ fontWeight: '0.8rem' }}>49 sold</StyledSpan>
+            </StyledRating>
+          </>
+
+          <Divider style={{ margin: '2.5rem 0' }} />
+
+          <QuantityWrapper>
+            <StyledSpan>Quantity : </StyledSpan>
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center'
+              }}
+            >
+              <Button icon={<MinusOutlined />} onClick={handleDecrease} disabled={!(quantity - 1)}></Button>
+              <Quantity>{quantity}</Quantity>
+              <Button icon={<PlusOutlined />} onClick={handleIncrease}></Button>
+            </div>
+            <StyledSpan> 499 pieces available</StyledSpan>
+          </QuantityWrapper>
+
+          <AddOrBuy>
+            <StyledButton>Add to cart</StyledButton>
+            <StyledButton>Buy it now</StyledButton>
+            <Button
+              onClick={handleLike}
+              size='large'
+              shape='circle'
+              icon={<HeartOutlined />}
+              style={{
+                backgroundColor: like ? 'red' : '#EEEEEE'
+              }}
+            />
+          </AddOrBuy>
+
+          <Divider style={{ margin: '2.5rem 0' }} />
+
+          <StyledCategory>
+            Category:
+            <StyledSpan style={{ fontWeight: 400 }}>20% off</StyledSpan>
+            <StyledSpan style={{ fontWeight: 400 }}>49% off</StyledSpan>
+            <StyledSpan style={{ fontWeight: 400 }}>Alex remote</StyledSpan>
+          </StyledCategory>
+
+          <StyledShare>
+            Share:
+            <StyledSpan style={{ fontWeight: 400 }}>
+              <GoogleOutlined />
+            </StyledSpan>
+            <StyledSpan style={{ fontWeight: 400 }}>
+              <FacebookOutlined />
+            </StyledSpan>
+            <StyledSpan style={{ fontWeight: 400 }}>
+              <WhatsAppOutlined />
+            </StyledSpan>
+          </StyledShare>
+        </ProductInfo>
+      </ProductWrapper>
+
+      <>
+        <TabsCustom
+          defaultActiveKey='1'
+          centered
+          items={[
+            {
+              label: <ButtonCustom>Description</ButtonCustom>,
+              key: '1',
+              children: <Description description={data?.data.description} />
+            },
+            {
+              label: <ButtonCustom>Reviews</ButtonCustom>,
+              key: '2',
+              children: <Reviews reviews={data?.data.reviews} />
+            }
+          ]}
+        />
+      </>
+
+      <RelatedProduct listProduct={data?.data} />
+    </ContentWrapper>
+  )
 }
 
 export default ProductDetails
